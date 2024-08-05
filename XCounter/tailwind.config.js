eaf558075b79bc4417cfa33ef54e2a303e74d5d8
Sudharshan Bommasani <sudharshan.bommasani@capgemini.com>
2024-08-05 22:56:10 +0530

    xcounter tailwindcss

---
+++ XCounter/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+      colors:{
+        primary: 'oklch(40% 0.23 2)'
+      }
+    },
+    plugins: [],
+  };
+  
