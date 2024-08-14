e6153a12c8a2bcbda28d18eeb26660ba82d8b064
Sudharshan Bommasani <sudharshan.bommasani@capgemini.com>
2024-08-14 21:54:56 +0530

    xcountries search

--- XCountriesSeacrh/src/App.js
+++ XCountriesSeacrh/src/App.js
@@ -22,7 +22,7 @@ function App() {
    * had a center array -parent
    * had dynamic array as per 
    */
-  const getCountries =useCallback(async()=>{
+  const getCountries =async()=>{
    
     // .then(response=>response.json())
     // .then(data=>setPlaceholder(data))
@@ -32,7 +32,8 @@ function App() {
 
     if(response.status ===200){
       const data = await response.json()
-      setPlaceholder(data)
+      setPlaceholder(data)// fro filtering
+      setCountries(data) //for maping
       console.log("Fetch successful")
     }
     else{
@@ -42,30 +43,23 @@ function App() {
     catch(error){
       console.error("API failed")
     }
-  },[])
-  const debounce = (event, delay)=>{
-    if(timerId.current){
-       clearTimeout(timerId.current)
-    }
-    timerId.current = setTimeout(()=>{setSearchInput(event.target.value)}, delay)
   }
-  const filterHandler =useCallback((input)=>{
+  // const debounce = (event, delay)=>{
+  //   if(timerId.current){
+  //      clearTimeout(timerId.current)
+  //   }
+  //   timerId.current = setTimeout(()=>{setSearchInput(event.target.value)}, delay)
+  // }
+  const filterHandler =(event)=>{
+    let value = event.target.value.toLowerCase()
     const filterObj = placeholder.filter(country=>{
-      if(country.name.toLowerCase().includes(input)){
+      if(country.name.toLowerCase().includes(value)){
         return country
       }
+      return "";
     })
-    return filterObj
-  },[placeholder])
-
- useEffect(()=>{
-     setCountries(filterHandler(seachInput.toLowerCase()))
- }, [seachInput,filterHandler])
-
-
- useEffect(()=>{
-  setCountries(placeholder)
-},[placeholder])
+    setCountries(filterObj)
+  }
 
   useEffect(()=>{
     getCountries()
@@ -73,7 +67,7 @@ function App() {
 
   return (
     <div className="App">
-        <div className='input-container'> <input className='input' type='text' placeholder='Sreach country name or code' onChange={(event)=>debounce(event, 500)}/></div>
+        <div className='input-container'> <input className='input' type='text' placeholder='Sreach country name or code' onChange={filterHandler}/></div>
         <div className="countryCard">
         {countries.map((country,index)=><CountryCard name={country.name} flag={country.flag} abbr= {country.abbr} key={`${country.name} ${index}`}/>)}    
         </div>   
